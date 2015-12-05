ab773906e7584db88a3fe35c5f5b75846929ebc8
Robert Hill <codingbyrobert@gmail.com>
2015-12-04 16:35:55 -0800

    preview working

--- scripts/secret.js
+++ scripts/secret.js
@@ -7,7 +7,20 @@ preveiw.getFormInfo = function(){
     var formAuthorUrl = $('#formAuthorUrl').val();
     var formCategory = $('#formCategory').val();
     var formArtBody = $('#formArtBody').val();
+    var today = new Date();
     console.log(formHeading + ' ' + formAuthor + ' ' + formAuthorUrl + ' ' + formCategory+ ' ' + formArtBody );
+
+    //var $data = $('.template').clone();
+    // $data.removeClass('template');
+    // ('#preview').find().attr('id', this.heading);
+    $('#preview').find('.heading').html("<p>" + formHeading  + "</p>");
+    $('#preview').find('.author').html("<p>" + formAuthor + "<p>");
+    $('#preview').find('.url').html("<p>" + formAuthorUrl + "</p");
+    $('#preview').find('.artbody').html(formArtBody);
+    $('#preview').find('.pub').html("<p>" + today + "</p>");
+    $('#preview').find('.cat').html("<p>" + formCategory + "</p>");
+
+    //$('#preview').append($data);
   });
   // e.preventDefult();
 
