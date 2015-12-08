6bfc7294822f149c1469b9e8e29aff0c70d81988
Mike <mikekh@uw.edu>
2015-12-07 16:42:57 -0800

    added handlebar temple in script at the top

--- scripts/article.js
+++ scripts/article.js
@@ -7,18 +7,23 @@ var Article = function(props) {
   this.category = props.category;
 };
 Article.prototype.toHTML = function() {
-  var $data = $('.template').clone();
-  $data.removeClass('template');
-  $data.addClass('article');
-  $data.attr('id', this.heading);
-  $data.find('.heading').html("<p>" + this.heading  + "</p>");
-  $data.find('.author').html("<p>" + this.author + "<p>");
-  $data.find('.url').html("<p>" + this.authorUrl + "</p");
-  $data.find('.artbody').html(this.artbody);
-  $data.find('.pub').html("<p>" + this.publishedOn + "</p>");
-  $data.find('.cat').html("<p>" + this.category + "</p>");
-  $data.find('.artbody').append('<button class = "button">' + 'Show More' + '</button>');
-  $('main').append($data);
+  // var $data = $('.artTemplate').clone();
+  // $data.removeClass('template');
+  // $data.addClass('article');
+  // $data.attr('id', this.heading);
+  // $data.find('.heading').html("<p>" + this.heading  + "</p>");
+  // $data.find('.author').html("<p>" + this.author + "<p>");
+  // $data.find('.url').html("<p>" + this.authorUrl + "</p");
+  // $data.find('.artbody').html(this.artbody);
+  // $data.find('.pub').html("<p>" + this.publishedOn + "</p>");
+  // $data.find('.cat').html("<p>" + this.category + "</p>");
+  // $data.find('.artbody').append('<button class = "button">' + 'Show More' + '</button>');
+
+  var source = $('#post-template').html();
+  var template = Handlebars.compile(source);
+  var html = template(this)
+  $('main').append(html);
+
 };
 
 
