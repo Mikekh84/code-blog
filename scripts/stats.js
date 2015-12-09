bc0d944f2cb0f8588161aa8c799e80fa16c48a85
Mike <mikekh@uw.edu>
2015-12-09 09:11:47 -0800

    # of articles works. Started unique authors

--- scripts/stats.js
+++ scripts/stats.js
@@ -4,12 +4,21 @@ var statData = '';
 stats.getData = function () {
   $.getJSON('scripts/hackerIpsum.json', function(data) {
     statData = data;
-    console.log(data);
-  }).done(numArticles);
+    // console.log(data);
+  }).done(numArticles)
+    .done(numAuthors);
 };
 
 
 function numArticles() {
   $('#stats').html('<p> Number of Articles:' + statData.length + '</p>');
 };
+
+function numAuthors() {
+    console.log('this worked');
+  };
+
+
+
+
 stats.getData();
